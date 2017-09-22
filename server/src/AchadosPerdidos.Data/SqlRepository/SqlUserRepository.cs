61d260070602ffe9c9995c35fa3aba9ad369888f
Lorrann Corrêa <lorrann.correa@esx.com.br>
2017-09-22 14:31:39 -0300

    ajustes nos diretorios e criacao do front end com vue cli

---
+++ server/src/AchadosPerdidos.Data/SqlRepository/SqlUserRepository.cs
@@ -0,0 +1,56 @@
+﻿using AchadosPerdidos.Business.Model;
+using AchadosPerdidos.Business.Repository;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace AchadosPerdidos.Data.SqlRepository
+{
+    public class SqlUserRepository : IUserRepository
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["AchadosPerdidosDB"].ConnectionString;
+
+        public void Create(User user)
+        {
+            var sqlConnection = new SqlConnection(connectionString);
+            var command = new SqlCommand("Insert into [AchadosPerdidos].[dbo].[User](name, fone, mail, cpf, password) " +
+                "values(@name, @fone, @mail, @cpf, @password)", sqlConnection);
+            command.Parameters.AddWithValue("@name", user.Name);
+            command.Parameters.AddWithValue("@fone", user.Fone);
+            command.Parameters.AddWithValue("@mail", user.Mail);
+            command.Parameters.AddWithValue("@cpf", user.CPF);
+            command.Parameters.AddWithValue("@password", user.Password);
+
+            sqlConnection.Open();
+            command.ExecuteNonQuery();
+            command.Clone();
+            sqlConnection.Close();
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            var sqlConnection = new SqlConnection(connectionString);
+            var command = new SqlCommand("SELECT name, fone, mail, cpf FROM [AchadosPerdidos].[dbo].[User]", sqlConnection);
+            SqlDataReader userReturn = null;
+
+            sqlConnection.Open();
+            userReturn = command.ExecuteReader();
+
+            IList<User> userList = new List<User>();
+
+            while (userReturn.Read())
+            {
+                User user = new User();
+
+                user.Name = userReturn[0].ToString();
+                user.Fone = userReturn[1].ToString();
+                user.Mail = userReturn[2].ToString();
+                user.CPF = userReturn[3].ToString();
+
+                userList.Add(user);
+            }
+            sqlConnection.Close();
+            return userList;
+        }
+    }
+}
