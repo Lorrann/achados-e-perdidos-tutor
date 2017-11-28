2a4d464a98015d3f2c50212cba08fdf45eece70b
Lorrann Corrêa <lorrann.correa@esx.com.br>
2017-11-28 19:11:06 -0200

    Criando nova API

---
+++ server/src/AchadosPerdidos.WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using AchadosPerdidos.Business.Business;
+using AchadosPerdidos.Business.Model;
+using AchadosPerdidos.Data.SqlRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AchadosPerdidos.WebAPI_.Controllers
+{
+    public class ProductController : ApiController
+    {
+		public ProductBusiness ProductBusiness { get; private set; }
+
+		public ProductController()
+		{
+			this.ProductBusiness = new ProductBusiness(new SqlProductRepository());
+		}
+
+		// POST: api/User
+		public void Post([FromBody]Product product)
+		{
+			this.ProductBusiness.Create(product);
+		}
+	}
+}
---
+++ server/src/AchadosPerdidos.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AchadosPerdidos.WebAPI_.WebApiApplication" Language="C#" %>
